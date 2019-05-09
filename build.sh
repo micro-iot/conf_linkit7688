c6f074e100bf9ee68dbc153eec8672c68f2489ab
micro-iot <y.micro.iot@googlemail.com>
2019-05-10 00:08:55 +0200

    Added patch application check.

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 if [ ! $(basename $(pwd)) == "poky" ]; then echo "Error! Must be run inside poky directory!"; exit 1; fi
 
+git apply --check ./meta-linkit7688/recipes-kernel/swconfig/micro-iot-poky-patch-for-swconfig/0001-Added-user-headers-in-kernel-class.patch &&\
 git am ./meta-linkit7688/recipes-kernel/swconfig/micro-iot-poky-patch-for-swconfig/0001-Added-user-headers-in-kernel-class.patch
