49a5b0c859c71793bad8d90ef1e27352c50e27b5
micro-iot <y.micro.iot@googlemail.com>
2019-05-11 02:01:26 +0200

    Added build step to build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ if [ ! $(basename $(pwd)) == "poky" ]; then echo "Error! Must be run inside poky
 
 git apply --check ./meta-linkit7688/recipes-kernel/swconfig/micro-iot-poky-patch-for-swconfig/0001-Added-user-headers-in-kernel-class.patch &&\
 git am ./meta-linkit7688/recipes-kernel/swconfig/micro-iot-poky-patch-for-swconfig/0001-Added-user-headers-in-kernel-class.patch
+
+. oe-init-build-env build
+bitbake micro-iot-basic-image
